2588cb705c9d773a11bda7ce6e5d423cd62b5fda
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2024-06-24 12:10:26 -0300

    Standard handling for other errors with Alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,6 +1,7 @@
 import { HttpEvent, HttpHandlerFn, HttpRequest } from "@angular/common/http";
 import { inject } from "@angular/core";
 import { Router } from "@angular/router";
+import { AlertController } from "@ionic/angular";
 import { Observable, catchError, of } from "rxjs";
 import { StorageService } from "src/app/service/storage.service";
 
@@ -10,6 +11,7 @@ export function errorInterceptor(
 
     let storage = inject(StorageService);
     let router = inject(Router);
+    let alertCtrl = inject(AlertController);
 
     return next(req)
         .pipe(
@@ -27,12 +29,15 @@ export function errorInterceptor(
 
                 switch (errorObj.status) {
                     case 401:
-                        handle401(storage, router);
+                        handle401(storage, router, alertCtrl);
                         break;
 
                     case 403:
                         handle403(storage, router);
                         break;
+
+                    default:
+                        handleDefaultEror(errorObj, alertCtrl);
                 }
 
                 return of(errorObj);
@@ -40,16 +45,62 @@ export function errorInterceptor(
         )
 }
 
-function handle401(storage: StorageService, router: Router) {
-    console.log('redirecionou pelo interceptor')
-
+function handle401(storage: StorageService, router: Router, alertCtrl: AlertController) {
+    const alertProps: AlertProps = {
+        header: 'Erro 401: falha de autenticação',
+        message: 'Email ou senha incorretos',
+        backdropDismiss: false,
+        buttons: [
+            {
+                text: 'Ok'
+            }
+        ]
+    }
+    presentAlert(alertCtrl, alertProps);
     storage.setLocalUser(null);
     router.navigate(['home']);
 }
 
 function handle403(storage: StorageService, router: Router) {
-    console.log('redirecionou pelo interceptor')
-
     storage.setLocalUser(null);
     router.navigate(['home']);
+}
+
+function handleDefaultEror(errorObj: any, alertCtrl: AlertController) {
+    const alertProps: AlertProps = {
+        header: 'Erro ' + errorObj.status + ': ' + errorObj.error,
+        message: errorObj.message,
+        backdropDismiss: false,
+        buttons: [
+            {
+                text: 'Ok'
+            }
+        ]
+    }
+    presentAlert(alertCtrl, alertProps);
+}
+
+async function presentAlert(alertCtrl: AlertController, alertProps: AlertProps) {
+    const alert = await alertCtrl.create({
+        header: alertProps.header,
+        message: alertProps.message,
+        backdropDismiss: alertProps.backdropDismiss,
+        buttons: [
+            {
+                text: alertProps.buttons[0].text
+            }
+        ]
+    });
+    await alert.present();
+}
+
+type AlertProps = {
+    header: string;
+    message: string;
+    backdropDismiss: boolean;
+    buttons: [
+        {
+            text: string
+        }
+    ]
 }
